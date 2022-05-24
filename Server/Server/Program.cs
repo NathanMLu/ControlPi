1ab5a407021590118acba2abbba40f1a42f24971
Nathan Lu <nathanlumax@gmail.com>
2022-05-24 17:08:24 -0500

    Created server and deployed to somee

---
+++ Server/Server/Program.cs
@@ -0,0 +1,25 @@
+public class Program {
+	private bool ledStatus;
+
+	private bool GetLedStatus() {
+		return ledStatus;
+	}
+
+	private bool ToggleLed() {
+		ledStatus = !ledStatus;
+		return ledStatus;
+	}
+
+
+	public static void Main(string[] args) {
+		var builder = WebApplication.CreateBuilder(args);
+		var app = builder.Build();
+		var program = new Program();
+
+		app.MapGet("/", () => { return DateTime.Now.ToString(); });
+		app.MapGet("/ledStatus", () => program.GetLedStatus());
+		app.MapGet("/toggleLed", () => program.ToggleLed());
+
+		app.Run();
+	}
+}
