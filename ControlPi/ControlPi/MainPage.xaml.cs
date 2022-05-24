817192125195faa41bfbb3a9c24ef95c930c4022
Nathan Lu <nathanlumax@gmail.com>
2022-05-24 16:57:46 -0500

    update get request from controller for real time updating

--- ControlPi/ControlPi/MainPage.xaml.cs
+++ ControlPi/ControlPi/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -11,11 +14,28 @@ namespace ControlPi {
 			InitializeComponent();
 		}
 
-		private int count = 0;
+		public void ToggleLed(object sender, System.EventArgs e) {
+			var button = (Button)sender;
+			button.Text = "Loading...";
+			
+			var url = "http://pi.somee.com/toggleled";
+			var request = WebRequest.Create(url);
+			request.Method = "GET";
 
-		void Handle_Clicked(object sender, System.EventArgs e) {
-			count += 3;
-			((Button)sender).Text = $"You clicked {count} times.";
+			var webResponse = request.GetResponse();
+			var webStream = webResponse.GetResponseStream();
+		
+			var reader = new StreamReader(webStream);
+			var data = reader.ReadToEnd();
+
+			button.Text = data;
+
+			// if (result == "1") {
+			// 	button.Text = "LED is on";
+			// } else {
+			// 	button.Text = "LED is off";
+			//
+			// button.Text = response.ToString();
 		}
 	}
 }
