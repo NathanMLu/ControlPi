3901758f4ea8da6ea33ae7412a4a148362cc2373
NathanMLu <nathanlumax@gmail.com>
2022-05-24 23:08:20 -0500

    updated led on cycle

--- Pi/Program.cs
+++ Pi/Program.cs
@@ -1,5 +1,26 @@
-﻿class Program {
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+class Program {
+    private void ToggleLed(int pin) {
+
+        var controller = new GpioController();
+        controller.OpenPin(pin, PinMode.Output);
+
+        while (true) {
+            controller.Write(pin, PinValue.High);
+            Thread.Sleep(500);
+            controller.Write(pin, PinValue.Low);
+            Thread.Sleep(500);
+        }
+    }
+
     static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+        var program = new Program();
+        int pin = 17;
+        program.ToggleLed(pin);
+        
+        Console.WriteLine("Toggling LED: " + pin);
     }
 }
