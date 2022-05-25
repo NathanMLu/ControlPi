44b05571a5c50a40fcbc5d2a99def79b03df9f03
NathanMLu <nathanlumax@gmail.com>
2022-05-24 23:28:03 -0500

    updated the listener for the api

--- Pi/Program.cs
+++ Pi/Program.cs
@@ -1,26 +1,56 @@
 ﻿using System;
 using System.Device.Gpio;
 using System.Threading;
+using System.Net;
 
 class Program {
-    private void ToggleLed(int pin) {
+    private async void ListenLed(int pin) {
+        Console.WriteLine("Starting to Listen");
 
+        // Pin Controller
         var controller = new GpioController();
         controller.OpenPin(pin, PinMode.Output);
 
+        Console.WriteLine("Still Listening");
+
+        // Request
+        // var url = "http://pi.somee.com/ledStatus";
+        // var request = WebRequest.Create(url);
+        // request.Method = "GET";
+
         while (true) {
+            // var url = "http://pi.somee.com/ledStatus";
+            // var client = new HttpClient();
+            // var response = await client.GetAsync(url);
+            // var content = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine("Hello There");
+            // Console.WriteLine(content);
+
+
+            // var response = request.GetResponse();
+            // var stream = response.GetResponseStream();
+            // var reader = new StreamReader(stream);
+            // var data = reader.ReadToEnd();
+
+            // if (data == "true") {
+            //     controller.Write(pin, PinValue.High);
+            // } else {
+            //     controller.Write(pin, PinValue.Low);
+            // }
+            // Thread.Sleep(1000);
+
             controller.Write(pin, PinValue.High);
-            Thread.Sleep(500);
+            Thread.Sleep(1000);
             controller.Write(pin, PinValue.Low);
-            Thread.Sleep(500);
+            Thread.Sleep(1000);
         }
     }
 
     static void Main(string[] args) {
         var program = new Program();
         int pin = 17;
-        program.ToggleLed(pin);
-        
+        program.ListenLed(pin);
+
         Console.WriteLine("Toggling LED: " + pin);
     }
 }
