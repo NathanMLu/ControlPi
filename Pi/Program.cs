10415905553be1484194026c8c5e0c298fa58f77
Nathan Lu <nathanlumax@gmail.com>
2022-05-24 23:38:47 -0500

    Fixed request

--- Pi/Program.cs
+++ Pi/Program.cs
@@ -7,19 +7,20 @@ class Program {
     private void ListenLed(int pin) {
 
         // Pin Controller
-        var controller = new GpioController();
-        controller.OpenPin(pin, PinMode.Output);
+        // var controller = new GpioController();
+        // controller.OpenPin(pin, PinMode.Output);
 
         // Request
         var url = "http://pi.somee.com/ledStatus";
-        var request = WebRequest.Create(url);
-        request.Method = "GET";
-        var response = request.GetResponse();
 
         while (true) {
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+            var response = request.GetResponse();
             var stream = response.GetResponseStream();
             var reader = new StreamReader(stream);
             var data = reader.ReadToEnd();
+            
             Console.WriteLine(data);
 
             // if (data == "true") {
