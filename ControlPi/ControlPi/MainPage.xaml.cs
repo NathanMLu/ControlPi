190e8663721701e0f8ad017b83b52e35252a7b36
Nathan Lu <nathanlumax@gmail.com>
2022-05-26 13:40:08 -0500

    Sync UI in real time on android app

--- ControlPi/ControlPi/MainPage.xaml.cs
+++ ControlPi/ControlPi/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -12,18 +13,52 @@ namespace ControlPi {
 	public partial class MainPage : ContentPage {
 		public MainPage() {
 			InitializeComponent();
+			// Run update UI on separate thread
+			Thread t = new Thread(UpdateUi);
+			t.Start();
+		}
+
+		private void UpdateUi() {
+			Button button = this.FindByName<Button>("ToggleButton");
+			while (true) {
+				try {
+					var webClient = new WebClient();
+					var response = webClient.UploadValues("http://pi.somee.com/api/ledstatus/", "POST",
+						new NameValueCollection());
+					var responseString = Encoding.Default.GetString(response);
+
+					if (responseString == "true") {
+						Device.BeginInvokeOnMainThread(() => {
+							button.Text = "LED IS ON";
+						});
+					}
+					else {
+						Device.BeginInvokeOnMainThread(() => {
+							button.Text = "LED IS OFF";
+						});
+					}
+				}
+				catch (System.Exception) {
+					Console.WriteLine("Error");
+					throw;
+				}
+
+				Thread.Sleep(1000);
+			}
 		}
 
 		public void ToggleLed(object sender, System.EventArgs e) {
 			var button = (Button) sender;
 
 			var webClient = new WebClient();
-			var response = webClient.UploadValues("http://pi.somee.com/api/toggleled/", "POST", new NameValueCollection());
+			var response =
+				webClient.UploadValues("http://pi.somee.com/api/toggleled/", "POST", new NameValueCollection());
 			var responseString = Encoding.Default.GetString(response);
 
 			if (responseString == "true") {
 				button.Text = "LED IS ON";
-			} else {
+			}
+			else {
 				button.Text = "LED IS OFF";
 			}
 		}
