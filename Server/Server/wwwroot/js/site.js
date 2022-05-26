63a650478b6ab892f11ead16b455119124b0dcc2
Nathan Lu <nathanlumax@gmail.com>
2022-05-26 01:01:16 -0500

    hid somee advertisement FINALLY

--- Server/Server/wwwroot/js/site.js
+++ Server/Server/wwwroot/js/site.js
@@ -9,22 +9,35 @@
     }
 }
 
-function removeSomee() {
-    $("center").each(function () {
-        if ($(this).html() === '<a href="http://somee.com">Web hosting by Somee.com</a>') {
-            $(this).next().remove();
-            $(this).next().next().remove();
-            $(this).next().next().next().remove();
-            $(this).remove();
-
-            return false;
-        }
-    });
+function SomeeAdsRemover() {
+    console.log("removed?");
+    $('script[language="JavaScript"]').remove();
+    $('script[src="http://ads.mgmt.somee.com/serveimages/ad2/WholeInsert5.js"]').remove();
+    $("div[style='opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;']").remove();
+    $("div[style='margin: 0px; padding: 0px; left: 0px; width: 100%; height: 65px; right: 0px; bottom: 0px; display: block; position: fixed; z-index: 2147483647; opacity: 0.9; background-color: rgb(32, 32, 32);']").remove();
+    $("div[onmouseover='S_ssac();']").remove();
+    $("center").remove();
+    $("div[style='height: 65px;']").remove();
+    // $("center").each(function () {
+    //     if ($(this).html() === '<a href="http://somee.com">Web hosting by Somee.com</a>') {
+    //         
+    //         $('script#last-script').nextAll('div').remove(); // last tag before </body>   
+    //         $(this).next().remove();
+    //         $(this).next().next().remove();
+    //         $(this).next().next().next().remove();
+    //         $(this).remove();
+    //
+    //         return false;
+    //     }
+    // });
 }
+
+// function adsRemover() {
+//     $('body > div:last-child').remove();
+// }
 
 function main() {
     $(document).ready(function () {
-        removeSomee();
         let button = $(".toggle");
         
         button.click(function () {
